73658654fc9450bd1996172e8b9b013de8d2a22e
Nathan S. Watson-Haigh <nathan.haigh@acpfg.com.au>
2014-05-27 09:59:47 +0930

    Added ability to add users and provide info on adding public SSH keys to the server

--- openstack_api_vm/setup_master_api_vm.sh
+++ openstack_api_vm/setup_master_api_vm.sh
@@ -12,3 +12,31 @@ pip install python-novaclient python-swiftclient python-keystoneclient python-gl
 # Install parallel-ssh
 apt-get install -y pssh
 
+# For each user that needs to interact with the NeCTAR Research Cloud, create a user account and generate an SSH key
+PASSWORD_LENGTH=10
+users=(
+  user1
+  user2
+  user3
+  user4
+)
+echo "Setting up users:"
+for user in "${users[@]}"; do
+  password=$(< /dev/urandom tr -dc _A-Z-a-z-0-9 | head -c${1:-${PASSWORD_LENGTH}};echo;)
+
+  echo -ne "\t${user}:${password} ... "
+  useradd --shell /bin/bash --create-home ${user}
+  echo -e "${user}:${password}" | chpasswd
+  echo "DONE"
+done
+
+echo "Commands for copying your public SSH keys to this VM and disabling password login:"
+for user in "${users[@]}"; do
+  echo -e "\tssh-copy-id ${user}@$(ifconfig eth0 | awk -F'[: ]+' '/inet addr:/ {print $4}') && ssh ${user}@$(ifconfig eth0 | awk -F'[: ]+' '/inet addr:/ {print $4}') passwd --delete ${user}"
+done
+
+# Immediately copy the public key of each person that needs to login to this VM and disable the password login
+# ssh-copy-id user1@
+
+
+
